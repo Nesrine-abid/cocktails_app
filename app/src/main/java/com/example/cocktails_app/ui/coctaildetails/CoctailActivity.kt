eb9df95f22f9da8b86868c32284af9f8048f2389
Abid Nesrine <nesrine.abid@bordeaux-inp.fr>
2023-11-29 09:49:43 +0100

    tabBar and Fragments

--- app/src/main/java/com/example/cocktails_app/ui/coctaildetails/CoctailActivity.kt
+++ app/src/main/java/com/example/cocktails_app/ui/coctaildetails/CoctailActivity.kt
@@ -2,11 +2,38 @@ package com.example.cocktails_app.ui.coctaildetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.cocktails_app.R
+import com.example.cocktails_app.databinding.ActivityCoctailBinding
+import com.example.cocktails_app.databinding.ActivityMainBinding
+import com.example.cocktails_app.ui.categories.CategoriesFragment
+import com.example.cocktails_app.ui.ingredients.IngredientsFragment
+import com.example.cocktails_app.ui.search.SearchFragment
 
 class CoctailActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityCoctailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coctail)
+        binding = ActivityCoctailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(SearchFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.categories_button -> replaceFragment(CategoriesFragment())
+                R.id.ingredients_button -> replaceFragment(IngredientsFragment())
+                R.id.search_button -> replaceFragment(SearchFragment())
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+    private fun replaceFragment (fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
